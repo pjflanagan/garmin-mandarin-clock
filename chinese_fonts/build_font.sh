20fae373d19a04296976810b384278f419c5c402
Mark Kuo <starryalley@gmail.com>
2020-02-02 23:21:28 +1100

    add Chinese font artwork and the build script

---
+++ chinese_fonts/build_font.sh
@@ -0,0 +1,6 @@
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources/fonts/font -r 25
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources-round-218x218/fonts/font -r 25
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources-round-240x240/fonts/font -r 30
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources-round-260x260/fonts/font -r 35
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources-round-280x280/fonts/font -r 40
+python3 garmin-tilemapper/tilemapper.py -i [0-9].png 1[0-9].png 2[0-9].png 3[0-9].png -o ../resources-round-390x390/fonts/font -r 60
